2a1eaba2d935241a6afaaa14e69fd83ef48c2e68
EsraAbdirashid <israApdirashid@gmail.com>
2024-05-12 20:05:52 +0300

    Done

--- Backend/controllers/taskControllers.js
+++ Backend/controllers/taskControllers.js
@@ -1,3 +1,10 @@
 import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient ()
+export const newTask = async (req , res)=>{
+        const {title,description}=req.body
+        const newTasks = await prisma.tasks.create({
+                data:{title,description}
+        })
+        
+}
